9b22dd4295219e06acab90a5cbd2b5bf2fff7e07
Jessica Waltz <jessicawaltz93@gmail.com>
2020-09-28 10:43:51 -0400

    added garden format template

---
+++ src/components/body/garden.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./garden.css";
+
+class Garden extends Component{
+    render(){
+        return(
+            <div className="garden-container">
+                <div className="1x1y"></div>
+                <div className="1x2y"></div>
+                <div className="1x3y"></div>
+                <div className="1x4y"></div>
+                <div className="1x5y"></div>
+                <div className="2x1y"></div>
+                <div className="2x2y"></div>
+                <div className="2x3y"></div>
+                <div className="2x4y"></div>
+                <div className="2x5y"></div>
+                <div className="3x1y"></div>
+                <div className="3x2y"></div>
+                <div className="3x3y"></div>
+                <div className="3x4y"></div>
+                <div className="3x5y"></div>
+                <div className="4x1y"></div>
+                <div className="4x2y"></div>
+                <div className="4x3y"></div>
+                <div className="4x4y"></div>
+                <div className="4x5y"></div>
+                <div className="5x1y"></div>
+                <div className="5x2y"></div>
+                <div className="5x3y"></div>
+                <div className="5x4y"></div>
+                <div className="5x5y"></div>
+            </div>
+        )
+    }
+}
+export default Garden
---
+++ src/components/body/tools.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Tools extends Component{
+    render(){
+        return(
+            <div className="garden-tools-container">
+                
+            </div>
+        )
+    }
+}
+export default Tools
